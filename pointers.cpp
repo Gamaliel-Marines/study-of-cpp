3ed340b13e889ffddc7c1152f14970700c369aff
Gamaliel-Marines <gamaliel_projects@hotmail.com>
2023-07-14 14:50:41 -0600

    feat: poinetrs basics

--- pointers.cpp
+++ pointers.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 
+void getMinAndMax(int numbers[], int size, int* min, int* max) {
+for (int i = 1; i < size; i++) {
+if (numbers[i] > *max)
+*max = numbers[i];
+if (numbers[i] < *min)
+*min = numbers[i];
+}
+}
+
+
 int main()
 {
+    int numbers [5] = { 5,4, -2,29,6 };
+    int min = numbers [0];
+    int max = numbers [0];
+    getMinAndMax(numbers, 5, &min, &max);
+    std::cout << "min is " << min << std::endl; 
+    std::cout << "max is " << max << std::endl;
+
     int a;
     a = 10;
     int *p = &a;
